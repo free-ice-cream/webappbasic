228cf0e3e6b5965bcb53a14d8189ed4deaaf2581
db <talktodonnybronson@gmail.com>
2018-07-01 17:55:04 +0200

    several copy and style changes

--- client/main.js
+++ client/main.js
@@ -7,6 +7,7 @@ var foo = apiData.findOne({_id: apiId});
 var apiKey = foo.apiKey;
 var apiURL = foo.api;
 
+//TODO  
 // var url =  'https://free-ice-cream.appspot.com/v1/players/';
 // var api_key = "95e5d50a-5271-4ea0-a6fb-0165bdde92f5";
 // var options = {
